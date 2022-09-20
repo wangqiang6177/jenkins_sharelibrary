2156f0a36e5137199b3deb40de1e85774fdfe0c8
qiangwang146 <qiangwang146@creditease.cn>
2022-09-20 18:09:36 +0800

    test

---
+++ src/com/jk/library('sharelibrary@master').groovy
@@ -0,0 +1,174 @@
+library('sharelibrary@master')
+pipeline{
+//在任何可用的代理上执行Pipeline
+    agent any
+
+   environment {
+       //收件人信息
+       _receiverEamil="xxx01@qq.com,xxx02@qq.com"   
+       //sonar属性参数
+        _sonarProjectKey="AAA"
+        _sonarExclusions="**/*.js"
+        //部门简称，用于sonar搜索
+        _departmentName="SCM"
+       //workspace目录下的pom路径
+       _pomDir=""
+       //jdk版本
+       _jdkVersion="jdk1.8.0_77"
+       //maven版本
+       _mavenVersion="maven_3"   
+       
+   }
+
+    //声明使用的工具
+    tools {
+      jdk "${_jdkVersion}"
+      maven "${_mavenVersion}"
+    }
+    //定义连接系统配置中的gitlab名称，为了在post阶段访问gitlab添加评论
+    options {
+        timestamps()
+        gitLabConnection('gitlab')
+    }
+    stages{
+//        stage ('填写参数') {
+
+//        }
+        stage ('引用共享库 ') {
+            steps{
+                script{
+                    //通过groovy脚本名称直接调用
+                    log.info("TEST")
+                   }
+                }
+        }
+        stage ('代码克隆') {
+            steps{
+                //使用Merge before build功能，实现PreCheck CI流程
+                checkout([$class: 'GitSCM', branches: [[name: '*/$gitlabSourceBranch']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PreBuildMerge', options: [mergeRemote: 'origin', mergeTarget: '${gitlabTargetBranch}']]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '7c0324ba-3771-4c1a-9957-bd67e68c4c13', url: '$gitlabSourceRepoHttpUrl']]])
+                script{
+                    env.GIT_COMMIT_MSG = sh (script:'git log -1 --pretty=%B ${GIT_COMMIT}',returnStdout: true).trim()
+                }
+                script{
+				//完整commit id
+				env.GIT_COMMIT_ID = sh (script: 'git rev-parse HEAD ${GIT_COMMIT}',returnStdout: true).trim()
+				echo "commit_id:$GIT_COMMIT_ID"
+				echo "代码仓库地址:$gitlabSourceRepoHttpUrl"
+				echo "$env.gitlabMergeRequestAssignee $env.gitlabUserEmail "
+				echo "-----------------------"
+				echo "gitlabBranch                       $env.gitlabBranch                      "
+echo "gitlabSourceBranch                 $env.gitlabSourceBranch                "
+echo "gitlabActionType                   $env.gitlabActionType                  "
+echo "gitlabUserName                     $env.gitlabUserName                    "
+echo "gitlabUserUsername                 $env.gitlabUserUsername                "
+echo "gitlabUserEmail                    $env.gitlabUserEmail                   "
+echo "gitlabSourceRepoHomepage           $env.gitlabSourceRepoHomepage          "
+echo "gitlabSourceRepoName               $env.gitlabSourceRepoName              "
+echo "gitlabSourceNamespace              $env.gitlabSourceNamespace             "
+echo "gitlabSourceRepoURL                $env.gitlabSourceRepoURL               "
+echo "gitlabSourceRepoSshUrl             $env.gitlabSourceRepoSshUrl            "
+echo "gitlabSourceRepoHttpUrl            $env.gitlabSourceRepoHttpUrl           "
+echo "gitlabMergeRequestTitle            $env.gitlabMergeRequestTitle           "
+echo "gitlabMergeRequestDescription      $env.gitlabMergeRequestDescription     "
+echo "gitlabMergeRequestId               $env.gitlabMergeRequestId              "
+echo "gitlabMergeRequestIid              $env.gitlabMergeRequestIid             "
+echo "gitlabMergeRequestState            $env.gitlabMergeRequestState           "
+echo "gitlabMergedByUser                 $env.gitlabMergedByUser                "
+echo "gitlabMergeRequestAssignee         $env.gitlabMergeRequestAssignee        "
+echo "gitlabMergeRequestLastCommit       $env.gitlabMergeRequestLastCommit      "
+echo "gitlabMergeRequestTargetProjectId  $env.gitlabMergeRequestTargetProjectId "
+echo "gitlabTargetBranch                 $env.gitlabTargetBranch                "
+echo "gitlabTargetRepoName               $env.gitlabTargetRepoName              "
+echo "gitlabTargetNamespace              $env.gitlabTargetNamespace             "
+echo "gitlabTargetRepoSshUrl             $env.gitlabTargetRepoSshUrl            "
+echo "gitlabTargetRepoHttpUrl            $env.gitlabTargetRepoHttpUrl           "
+echo "gitlabBefore                       $env.gitlabBefore                      "
+echo "gitlabAfter                        $env.gitlabAfter                       "
+echo "gitlabTriggerPhrase                $env.gitlabTriggerPhrase               "
+				}
+                }
+        }
+        stage('编译打包'){
+            steps{
+                echo "Building..."
+                sh '''#!/bin/bash
+                cd ${WORKSPACE}/${_pomDir} && mvn clean package -DskipTests=true'''
+			}
+        }
+        stage('单元测试'){
+            steps{
+                echo "Testing..."
+				//先执行输出单元测试报告，再执行输出单元测试覆盖率报告
+                sh '''cd ${WORKSPACE}/${_pomDir} && mvn org.jacoco:jacoco-maven-plugin:prepare-agent surefire-report:report
+                mvn org.jacoco:jacoco-maven-plugin:report'''
+            }
+        }
+        stage ('静态代码检测') {
+            steps{
+				//sonar8.9指的是jenkins系统配置SonarQube Severs的名称
+                withSonarQubeEnv('sonar8.9'){
+                    sh '''cd ${WORKSPACE}/${_pomDir} && mvn sonar:sonar \
+                    -Dsonar.projectKey=${_sonarProjectKey} \
+                    -Dsonar.projectName=${_departmentName}_${JOB_NAME}_$BUILD_NUMBER \
+		            -Dsonar.projectDescription=$gitlabSourceRepoHttpUrl \
+                    -Dsonar.projectVersion=1.0 \
+                    -Dsonar.sourceEncoding=UTF-8 \
+                    -Dsonar.exclusions=${_sonarExclusions} '''
+                    //sh ''' cd ${WORKSPACE} && mvn sonar:sonar'''
+
+                }
+            }
+        }
+        stage("Quality Gate") {
+            steps{
+                timeout(time: 5, unit: 'MINUTES') { 
+                    script{
+                        sleep(5)
+                        def qg = waitForQualityGate() 
+                        if (qg.status != 'OK') {
+                            error "未通过Sonarqube的代码质量阈检查，请及时修改！failure: ${qg.status}"
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    post { 
+    		always{
+    			emailext body: '''${FILE, path="/data/jenkins/email-templates/mail_template.html"}''', 
+    			subject: '[Jenkins Build Notification] ${PROJECT_NAME} - Build # ${BUILD_NUMBER} - ${BUILD_STATUS}!', 
+    			to: "${_receiverEamil}"
+    		}
+    		success {
+    			//当此Pipeline成功时打印消息
+    			echo 'success'
+                //调用共享库函数
+                script{
+                    log.addgitComment("success")
+                }
+    		}   
+    		failure {
+    			//当此Pipeline失败时打印消息
+    			echo 'failure'
+                //调用共享库函数 
+                script{
+                    log.addgitComment("failed")
+                }
+    		}
+    		unstable {
+    			//当此Pipeline 为不稳定时打印消息
+    			echo 'unstable'		
+    		}
+    		aborted {
+    			//当此Pipeline 终止时打印消息
+    			echo 'aborted'	
+                updateGitlabCommitStatus(name: 'build', state: 'failed')
+    		}
+    		changed {
+    			//当pipeline的状态与上一次build状态不同时打印消息
+    			echo 'changed'			
+    		}        
+    	}
+}
---
+++ vars/hello.groovy
@@ -0,0 +1,4 @@
+#!groovy
+def call(String name="human"){
+  println "Hello,$name"
+}
